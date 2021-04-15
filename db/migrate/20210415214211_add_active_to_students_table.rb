f86af4f27e52595e57112765450e000637b45903
Dewey <hammersdrichard@gmail.com>
2021-04-15 16:15:49 -0700

    Done.

--- db/migrate/20210415214211_add_active_to_students_table.rb
+++ db/migrate/20210415214211_add_active_to_students_table.rb
@@ -1,5 +1,5 @@
 class AddActiveToStudentsTable < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :active, :boolean
+    add_column :students, :active, :boolean, default: false, null: false
   end
 end
